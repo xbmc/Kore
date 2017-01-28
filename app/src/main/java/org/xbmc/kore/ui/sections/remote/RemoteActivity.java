14d792a27891e197a2f67a379016b45a41d49a66
Joel Hedlund <joel@nsc.liu.se>
2017-01-29 00:17:22 +0100

    Add support for sharing SVTplay urls to "Play on Kodi".
    SVTplay is a free streaming service provided by the Swedish
    national television SVT.
    
    This feature requires the svtplay video plugin on Kodi.

--- app/src/main/java/org/xbmc/kore/ui/sections/remote/RemoteActivity.java
+++ app/src/main/java/org/xbmc/kore/ui/sections/remote/RemoteActivity.java
@@ -369,9 +369,13 @@ public class RemoteActivity extends BaseActivity
         final String videoId = getVideoId(videoUri);
         String videoUrl;
         if (videoId != null) {
-            videoUrl = "plugin://plugin.video." +
-                (videoUri.getHost().endsWith("vimeo.com") ? "vimeo" : "youtube") +
-                "/play/?video_id=" + videoId;
+            if (videoUri.getHost().endsWith("svtplay.se")) {
+                videoUrl = "plugin://plugin.video.svtplay/?url=%2Fvideo%2F" + videoId + "&mode=video";
+            } else if (videoUri.getHost().endsWith("vimeo.com")) {
+                videoUrl = "plugin://plugin.video.vimeo?video_id=" + videoId;
+            } else {
+                videoUrl = "plugin://plugin.video.youtube/play/?video_id=" + videoId;
+            }
 
         } else {
             videoUrl = videoUri.toString();
@@ -517,10 +521,13 @@ public class RemoteActivity extends BaseActivity
      * @return Youtube/Vimeo Video ID
      */
     private String getVideoId(Uri playuri) {
-        if (playuri.getHost().endsWith("youtube.com") || playuri.getHost().endsWith("youtu.be") || playuri.getHost().endsWith("vimeo.com")) {
+        if (playuri.getHost().endsWith("svtplay.se") || playuri.getHost().endsWith("youtube.com") || playuri.getHost().endsWith("youtu.be") || playuri.getHost().endsWith("vimeo.com")) {
             // We'll need to get the v= parameter from the URL
             final Pattern pattern;
-            if (playuri.getHost().endsWith("vimeo.com")) {
+            if (playuri.getHost().endsWith("svtplay.se")) {
+                pattern = Pattern.compile("^(?:https?:\\/\\/)?(?:www\\.)?svtplay\\.se\\/video\\/(\\d+)\\/.*",
+                        Pattern.CASE_INSENSITIVE);
+            } else if (playuri.getHost().endsWith("vimeo.com")) {
                 pattern = Pattern.compile("^(?:https?:\\/\\/)?(?:www\\.|player\\.)?vimeo\\.com\\/(?:.*\\/)?(\\d+)(?:\\?.*)?$",
                         Pattern.CASE_INSENSITIVE);
             }
