893f2c852cef8ccdfec0d06ee36325f7d0ad2d9a
Martijn Brekhof <m.brekhof@gmail.com>
2017-06-20 17:37:33 +0200

    Implemented a HighlightButton widget
    This can be used by buttons that need to be highlighted such as
    the shuffle and the mute buttons.

---
+++ app/src/main/java/org/xbmc/kore/ui/widgets/HighlightButton.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017 Martijn Brekhof. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.xbmc.kore.ui.widgets;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.support.v7.widget.AppCompatImageButton;
+import android.util.AttributeSet;
+
+import org.xbmc.kore.R;
+
+public class HighlightButton extends AppCompatImageButton {
+    private int colorFilter;
+
+    private boolean highlight;
+
+    public HighlightButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        Resources.Theme theme = context.getTheme();
+        TypedArray styledAttributes = theme.obtainStyledAttributes(new int[]{
+                R.attr.colorAccent});
+        colorFilter = styledAttributes.getColor(styledAttributes.getIndex(0),
+                                  context.getResources().getColor(R.color.accent_default));
+        styledAttributes.recycle();
+    }
+
+    public void setHighlight(boolean highlight) {
+        if (highlight) {
+            setColorFilter(colorFilter);
+        } else {
+            clearColorFilter();
+        }
+        this.highlight = highlight;
+    }
+
+    public boolean isHighlighted() {
+        return highlight;
+    }
+}
