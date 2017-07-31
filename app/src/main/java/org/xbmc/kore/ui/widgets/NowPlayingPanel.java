972bdc79eefd010b97e643d0dd135cc3d373d9b0
n123al <nicholas@hadjimina.com>
2017-07-31 20:59:27 +0300

    Universal Subtitles Options Dialog
    this commit fixes #375
    
    A class (app\src\main\java\org\xbmc\kore\ui\generic\SubtitlesOptionsDialog.java) was developed that handles the display of a dialog with options related to subtitles (Download subtitles, Sync subtitles, none). This class was used to add the subtitle button to the widget NowPlayingPanel and the RemoteFragment. Furthermore, any developer can use this class in order to implement a subtitle button to any activity.

--- app/src/main/java/org/xbmc/kore/ui/widgets/NowPlayingPanel.java
+++ app/src/main/java/org/xbmc/kore/ui/widgets/NowPlayingPanel.java
@@ -17,7 +17,9 @@
 package org.xbmc.kore.ui.widgets;
 
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,12 +32,26 @@ import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 import org.xbmc.kore.R;
 import org.xbmc.kore.jsonrpc.type.GlobalType;
+import org.xbmc.kore.ui.generic.SubtitlesOptionsDialog;
+import org.xbmc.kore.ui.sections.remote.RemoteActivity;
 import org.xbmc.kore.utils.UIUtils;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public class NowPlayingPanel extends SlidingUpPanelLayout {
+public class NowPlayingPanel extends SlidingUpPanelLayout
+        implements  SubtitlesOptionsDialog.SubtitlesOptionsDialogListener{
+
+    /**
+     * After subtitle option selected show the Remote control activity
+     *
+     */
+    @Override
+    public void onRemoteRequired() {
+        Intent launchIntent = new Intent(this.getContext(), RemoteActivity.class)
+                .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        this.getContext().startActivity(launchIntent);
+    }
 
     public interface OnPanelButtonsClickListener {
         void onPlayClicked();
@@ -49,12 +65,15 @@ public class NowPlayingPanel extends SlidingUpPanelLayout {
 
     private OnPanelButtonsClickListener onPanelButtonsClickListener;
 
+    private SubtitlesOptionsDialog subtitlesDialog;
+
     @InjectView(R.id.npp_title) TextView title;
     @InjectView(R.id.npp_details) TextView details;
     @InjectView(R.id.npp_poster) ImageView poster;
     @InjectView(R.id.npp_previous) ImageButton previousButton;
     @InjectView(R.id.npp_next) ImageButton nextButton;
     @InjectView(R.id.npp_play) ImageButton playButton;
+    @InjectView(R.id.npp_subtitles) ImageButton subtitlesButton;
     @InjectView(R.id.npp_progress_indicator) MediaProgressIndicator mediaProgressIndicator;
     @InjectView(R.id.npp_volume_level_indicator) VolumeLevelIndicator volumeLevelIndicator;
     @InjectView(R.id.npp_volume_mute) HighlightButton volumeMuteButton;
@@ -77,6 +96,8 @@ public class NowPlayingPanel extends SlidingUpPanelLayout {
     }
 
     private void initializeView(Context context) {
+        subtitlesDialog = new SubtitlesOptionsDialog((Activity)context);
+        subtitlesDialog.addListener(this);
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = inflater.inflate(R.layout.now_playing_panel, this);
         ButterKnife.inject(view);
@@ -182,6 +203,7 @@ public class NowPlayingPanel extends SlidingUpPanelLayout {
         shuffleButton.setOnClickListener(handleButtonClickListener);
         repeatModeButton.setOnClickListener(handleButtonClickListener);
         volumeMutedIndicatorButton.setOnClickListener(handleButtonClickListener);
+        subtitlesButton.setOnClickListener(handleButtonClickListener);
     }
 
     private OnClickListener handleButtonClickListener = new OnClickListener() {
@@ -211,6 +233,9 @@ public class NowPlayingPanel extends SlidingUpPanelLayout {
                     break;
                 case R.id.npp_volume_muted_indicator:
                     onPanelButtonsClickListener.onVolumeMutedIndicatorClicked();
+                    break;
+                case R.id.npp_subtitles:
+                    subtitlesDialog.Show();
                     break;
             }
         }
