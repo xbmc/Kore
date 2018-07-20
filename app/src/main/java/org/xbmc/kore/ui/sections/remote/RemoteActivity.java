xbmc/Kore

commit d11dbe68531f8e9f17cbbead11fa4a879eb89ee4

Fixes a possible race condition and starting multiple instances (#559)
The onDestroyView method is used to unregister the application observer
from the dialog. However the parent got called first which destroys the
view of the dialog. This could lead to Null Pointer Exceptions when
the listener tries to update the UI of the dialog right after it was
destroyed.

The VolumeControllerDialogFragmentListener was created twice as creation
was triggered on both the down and up event of the volume keys. This has
been changed to only trigger on down events.

app/src/main/java/org/xbmc/kore/ui/sections/remote/RemoteActivity.java
@@ -290,6 +290,9 @@ public class RemoteActivity extends BaseActivity
      */
     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
+        if (event.getAction() != KeyEvent.ACTION_DOWN)
+            return false;
+
         boolean handled = VolumeControllerDialogFragmentListener.handleVolumeKeyEvent(this, event);
 
         // Show volume change dialog if the event was handled and we are not in

app/src/main/java/org/xbmc/kore/ui/generic/VolumeControllerDialogFragmentListener.java
@@ -124,13 +124,12 @@ public class VolumeControllerDialogFragmentListener extends AppCompatDialogFragm
 
     @Override
     public void onDestroyView() {
-        super.onDestroyView();
-
         HostConnectionObserver hostConnectionObserver = hostManager.getHostConnectionObserver();
         if (hostConnectionObserver != null) {
             hostConnectionObserver.unregisterApplicationObserver(this);
         }
         unbinder.unbind();
+        super.onDestroyView();
     }
 
     private void registerObserver() {

commit 4a800da2f11ed7262254a76615bc489ab07cf987

Fix back button not exiting the app when on the remote activity
Hitting the back button in the remote activity was being ignored, thus preventing exiting the app. This issue was inadvertidely introduced in d11dbe68531f8e9f17cbbead11fa4a879eb89ee4

app/src/main/java/org/xbmc/kore/ui/sections/remote/RemoteActivity.java
@@ -290,16 +290,16 @@ public class RemoteActivity extends BaseActivity
      */
     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
-        if (event.getAction() != KeyEvent.ACTION_DOWN)
-            return false;
+        boolean handled = false;
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            handled = VolumeControllerDialogFragmentListener.handleVolumeKeyEvent(this, event);
 
-        boolean handled = VolumeControllerDialogFragmentListener.handleVolumeKeyEvent(this, event);
-
-        // Show volume change dialog if the event was handled and we are not in
-        // first page, which already contains a volume control
-        if (handled && (viewPager.getCurrentItem() != 0)) {
-            new VolumeControllerDialogFragmentListener()
-                    .show(getSupportFragmentManager(), VolumeControllerDialogFragmentListener.class.getName());
+            // Show volume change dialog if the event was handled and we are not in
+            // first page, which already contains a volume control
+            if (handled && (viewPager.getCurrentItem() != 0)) {
+                new VolumeControllerDialogFragmentListener()
+                        .show(getSupportFragmentManager(), VolumeControllerDialogFragmentListener.class.getName());
+            }
         }
         return handled || super.dispatchKeyEvent(event);
     }