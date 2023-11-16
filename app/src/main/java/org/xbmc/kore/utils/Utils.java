b77fc22371623f50d0ed53e24cc4a821ec58b28c
Synced Synapse <synced.synapse@gmail.com>
2023-11-16 16:13:19 +0000

    Upgrade to Android 13, Sdk Version 33 (#985)
    - Add notification permission to manifest, though it is not strickly needed as all notifications are attached to a MediaSession and those are exempt from Android's 13 new requirements on notification permissions
      https://developer.android.com/develop/ui/views/notifications/notification-permission
    - Use granular media permissions, switching from READ_EXTERNAL_STORAGE to READ_MEDIA_{IMAGES|VIDEO|AUDIO}
      https://developer.android.com/about/versions/13/behavior-changes-13#granular-media-permissions

--- app/src/main/java/org/xbmc/kore/utils/Utils.java
+++ app/src/main/java/org/xbmc/kore/utils/Utils.java
@@ -27,10 +27,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.text.TextUtils;
 
-import androidx.preference.PreferenceManager;
-
-import org.xbmc.kore.Settings;
-
 import java.util.List;
 import java.util.Locale;
 
@@ -50,6 +46,10 @@ public class Utils {
 
     public static boolean isSOrLater() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.S;
+    }
+
+    public static boolean isTiramisuOrLater() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU;
     }
 
     /**
